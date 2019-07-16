69b2913f023651080912bb900423b3eb4386ffbc
Matt McCarthy <mattmc318@gmail.com>
2019-07-15 19:14:14 -0500

    Updated home page

--- home/static/home/js/script.js
+++ home/static/home/js/script.js
@@ -1,9 +1,33 @@
 $(document).ready(function () {
+    // animate on chevron click
     $('.chevron').on('click touchend', function (event) {
         event.preventDefault();
 
         $('html, body').animate({
             scrollTop: $('.content').offset().top - 48,
         }, 1000);
+    });
+
+    // resize Google Maps widget
+    function resizeMap() {
+        var $map = $('#gmap-canvas');
+        var width = $(this).width();
+
+        if (width >= 576) {
+            $map.attr('width', 400).attr('height', 400);
+        } else if (width >= 360 && width < 576) {
+            $map.attr('width', 300).attr('height', 300);
+        } else  {
+            $map.attr('width', 200).attr('height', 200);
+        }
+    }
+
+    // window event listeners
+    resizeMap();
+    $(window).resize(function () {
+        resizeMap();
+    });
+    $(window).on('orientationchange', function () {
+        $(window).trigger('resize');
     });
 });
