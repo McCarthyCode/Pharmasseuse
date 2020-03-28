d8010a2c9892e2da985e5527244c329b2bd90731
Matt McCarthy <matt@mccarthywebdesign.com>
2020-03-28 17:35:35 -0500

    Added developer's site link in console

---
+++ home/static/home/js/global.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  // devloper's site link in console
+  console.log('This site brought to you by McCarthy Web Design.');
+  console.log('https://mccarthywebdesign.com/');
+});
